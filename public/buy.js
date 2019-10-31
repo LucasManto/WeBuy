2012311acd089a06970406299e1ca05ae76dc8c4
Lucas Mantovani <lucashmantovani@hotmail.com>
2019-10-31 12:49:26 -0300

    Updating: login/logout flow, add participant and add link

--- public/buy.js
+++ public/buy.js
@@ -3,31 +3,57 @@ $(document).ready(() => {
     let auth = firebase.auth();
     let firestore = firebase.firestore();
 
+    auth.onAuthStateChanged(user => {
+        if (!user) window.location.href = 'index.html'
+    })
+
     $("#btn-logout").click(() => {
-        // TODO:
-        //Logout
+        auth.signOut()
     });
 
-    //Get room name and use for header
-    var url_string = window.location.href.split("#!")[0]
-    var storeName = url_string.split("name=")[1];
-    $("#roomName").html(storeName)
+    let docSnapshot = null
+    let docData = null
+
+    async function loadUI() {
+        var url_string = window.location.href.split("#!")[0]
+
+        docSnapshot = await firestore.doc(`rooms/${url_string.split("name=")[1]}`).get()
+        docData = docSnapshot.data()
+
+        var storeName = docData.storeName
+        $("#roomName").html(storeName)
+    }
+
+    loadUI()
+
 
     $("#btn-addProduct").click(() => {
         var quantidadeProduto = document.getElementById("quantidadeProduto").value;
         quantidadeProduto = parseInt(quantidadeProduto)
-        if((quantidadeProduto >= 5) || (quantidadeProduto <= 0)){
+        if ((quantidadeProduto >= 5) || (quantidadeProduto <= 0)) {
             alert("Quantidade inválida!")
-        }else{
+        } else {
             // TODO:
             //CADASTRAR O PRODUTO NO BANCO
+            addLink(docSnapshot.id)
         }
     });
 
     $("#btn-listRooms").click(() => {
-        var newUrlPath = window.location.pathname
-        newUrlPath = newUrlPath.split("buy.html")[0]
-        newUrlPath = newUrlPath.concat("rooms.html")            
-        window.location.replace(newUrlPath)
+        window.location.href = 'rooms.html'
     });
+
+    async function addLink(roomId) {
+        const productLink = $('#urlProduto').val()
+
+        let productLinks = docData.productLinks
+        productLinks[auth.currentUser.uid].push(productLink)
+
+        await firestore.doc(`rooms/${roomId}`).update({
+            productLinks
+        })
+
+        alert('Você adicionou um produto à compra em grupo. Agora é só aguardar a entrega :)')
+        window.location.href = 'rooms.html'
+    }
 });
--- public/rooms.js
+++ public/rooms.js
@@ -2,6 +2,10 @@ $(document).ready(() => {
     let auth = firebase.auth()
     let firestore = firebase.firestore()
 
+    auth.onAuthStateChanged(user => {
+        if (!user) window.location.href = 'index.html'
+    })
+
     $("#div-create-room").hide();
     $("#btn-createRoom").show();
     $("#btn-listRooms").hide();
@@ -22,32 +26,30 @@ $(document).ready(() => {
     });
 
     $("#btn-signRoom").click(() => {
-        // TODO:
-        //createRoom()
+        createRoom()
     });
 
     $("#btn-logout").click(() => {
-        // TODO:
-        //Logout
+        auth.signOut()
     });
 
     //Load the slider elements
-    window.onload = function() {
-        var elems  = document.querySelectorAll("input[name=maxPartSlider]");
+    window.onload = function () {
+        var elems = document.querySelectorAll("input[name=maxPartSlider]");
         M.Range.init(elems);
     };
 
-    function createRoom() {
+    async function createRoom() {
         // TODO:
         // - Change maxParticipants, checkoutTime and store name variables to get values from fields
-        const maxParticipants = 5;
-        const checkoutTime = 3;
-        const storeName = 'growth';
+        const maxParticipants = document.getElementById('maxPart').value
+        const checkoutTime = $('#inputTime').val()
+        const storeName = $('#inputRoomName').val()
 
         // TODO:
         // - Add the actual number of participants on the room, default 1 on creation
         try {
-            firestore.collection('rooms').doc().set({
+            await firestore.collection('rooms').doc().set({
                 'maxParticipants': maxParticipants,
                 'checkoutTime': checkoutTime,
                 'storeName': storeName,
@@ -60,6 +62,8 @@ $(document).ready(() => {
                 'owner': auth.currentUser.uid,
                 'createdAt': Date.now()
             });
+            clearRoomsList()
+            listGetRooms()
         } catch (error) {
             throw error
         }
@@ -76,15 +80,18 @@ $(document).ready(() => {
         const roomDocumentSnapshot = await roomDocumentReference.get()
         roomData = await roomDocumentSnapshot.data()
 
+        if (roomData.participants.includes(auth.currentUser.uid))
+            return
+
         if (roomData.participants.length < roomData.maxParticipants) {
-            roomDocumentReference.update({
+            roomData.productLinks[auth.currentUser.uid] = []
+
+            await roomDocumentReference.update({
                 'participants': firebase.firestore.FieldValue.arrayUnion(auth.currentUser.uid),
-                'productLinks': {
-                    [auth.currentUser.uid]: []
-                }
+                productLinks: roomData.productLinks
             })
 
-            firestore.doc(`users/${auth.currentUser.uid}`).update({
+            await firestore.doc(`users/${auth.currentUser.uid}`).update({
                 activeRooms: firebase.firestore.FieldValue.arrayUnion(roomId)
             })
         }
@@ -93,60 +100,45 @@ $(document).ready(() => {
         }
     }
 
-    async function addLink(roomId) {
-        // TODO:
-        // - Check if link is from room store
-        const productLink = 'https://growth.com.br'
-
-        firestore.doc(`rooms/${roomId}`).update({
-            'productLinks': {
-                [auth.currentUser.uid]: firebase.firestore.FieldValue.arrayUnion(productLink)
-            }
-        })
-    }
-
-    // setInterval(() => {
-    //     if (new Date() > new Date(2019, 9, 28, 23, 1))
-    //         console.log('dsfkjndfkjnsd')
-    // }, 60000);
-
     async function listGetRooms() {
         let roomsDocumentReference = await getRooms();
-        const roomName = "roomId";
-        var actualId = 0;
         roomsDocumentReference.forEach(docReference => {
+            const roomId = docReference.id
             const firstPiece = "<div class=\"collection\" id=\"clt-rooms\"> <a href=\"#!\" id=\"storeRoom\" name=\""
             const secondPiece = "\" class=\"collection-item light-blue darken-4 white-text\" style=\"font-weight: bold;\">"
             const thirdPiece = "<i class=\"material-icons\" style=\"padding-left: 80%;\">group</i><b style=\"padding-left: 10px;\">"
             const forthPiece = "</b></a></div>"
-            
+
             //Create the room name with all attributes needed
-            var roomNameValue = roomName.concat(actualId.toString(), "#", docReference.data().storeName, "#", docReference.data().maxParticipants, "#",  docReference.data().actualParticipants)
-            
+            var roomNameValue = roomId.concat("#", docReference.data().storeName, "#", docReference.data().maxParticipants, "#", docReference.data().participants.length)
+
             //var finalRoom = firstPiece.concat(docReference.data().storeName, secondPiece, "/ ", docReference.data().maxParticipants, thirdPiece)
             var finalRoom = firstPiece.concat(roomNameValue, secondPiece, docReference.data().storeName, thirdPiece, "/ ", docReference.data().maxParticipants, forthPiece)
             $("#div-salas2").append(finalRoom)
-            actualId += 1
 
             // console.log(finalRoom)
             //console.log(roomName.concat(actualId.toString()))
         })
     }
 
-    $(document).on("click","#storeRoom",function(){
+    function clearRoomsList() {
+        document.getElementById('clt-rooms').innerHTML = ''
+    }
+
+    $(document).on("click", "#storeRoom", async function () {
         //console.log(this.name.split("#"));
         var roomAttributes = this.name.split("#");
-        if(parseInt(roomAttributes[2]) <= parseInt(roomAttributes[3])){
+        if (parseInt(roomAttributes[2]) <= parseInt(roomAttributes[3])) {
             alert("Perdão, esta sala já está lotada!")
-        }else{
+        } else {
             //Creating the custom url for different rooms
             //console.log(window.location.pathname)
+            await addParticipant(roomAttributes[0])
+
             var newUrlPath = window.location.pathname
             newUrlPath = newUrlPath.split("rooms.html")[0]
-            newUrlPath = newUrlPath.concat("buy.html?name=", roomAttributes[1])            
+            newUrlPath = newUrlPath.concat("buy.html?name=", roomAttributes[0])
             window.location.replace(newUrlPath)
-            // TODO:
-            //Increase the number of participants on the room by 1
         }
     });
 
